3c78cf1f21971698ecf8f845074257a4d467f5fe
meghnath <meghnath1782004@gmail.com>
2026-06-18 15:32:57 +0530

    chore: upgrade node base image from 20 to 22-alpine

--- landing-page/src/components/Hero.tsx
+++ landing-page/src/components/Hero.tsx
@@ -14,6 +14,7 @@ export default function Hero({ isAppLoading }: { isAppLoading?: boolean }) {
         return () => window.removeEventListener('scroll', handleScroll)
     }, [])
 
+    return (
         <section className="hero" id="hero">
 
             <motion.div
